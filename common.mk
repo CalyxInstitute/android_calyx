09df4f1db912e994395abdd548edb17d37a68395
Chirayu Desai <chirayudesai1@gmail.com>
2017-11-29 21:21:54 +0530

    Set owner for bootanimation copy

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
 PRODUCT_COPY_FILES += \
-	vendor/calyx/bootanimation.zip:system/media/bootanimation.zip
+	vendor/calyx/bootanimation.zip:system/media/bootanimation.zip:google
 
 DEVICE_PACKAGE_OVERLAYS += vendor/calyx/overlay/common
 
