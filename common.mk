418b66eea30f8e4c31f50cedb7f4f4256724884c
Chirayu Desai <chirayudesai1@gmail.com>
2017-12-11 18:06:02 +0530

    Move outside vendor/

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 PRODUCT_COPY_FILES += \
-	vendor/calyx/bootanimation.zip:system/media/bootanimation.zip:google
+	calyx/bootanimation.zip:system/media/bootanimation.zip
 
-DEVICE_PACKAGE_OVERLAYS += vendor/calyx/overlay/common
+DEVICE_PACKAGE_OVERLAYS += calyx/overlay/common
 
 PRODUCT_PACKAGES += \
 	Updater
